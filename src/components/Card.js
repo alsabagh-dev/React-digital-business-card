24509e15eb39afc205c399039a44c5f922db0ecf
Abdelaziz Alsabagh <dev.alsabagh@gmail.com>
2022-04-04 16:22:33 +0200

    add main section

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,10 +2,17 @@ import Article from "./Article";
 import Main from "./Main";
 
 const Card = function(props){
+    const info = props.info;
     return(
         <div className="card">
-            <img className="card-img" src="/alsabagh.png" alt="Alsabagh Pic"/>
-            <Main/>
+            <img className="card-img" src={info.Avatar} alt={info.Name}/>
+            <Main 
+                name={info.Name} 
+                title={info.Title}
+                site={info.Website}
+                linkedIn={info.LinkedIn}
+                email={info.Email}
+                />
             <Article/>
             <Article/>
         </div>
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,28 @@
-const Main = function(props){
-    return(
+import MailIcon from "../icons/MailIcon";
+import LinkedInIcon from "../icons/LinkedInIcon";
+
+const Main = function (props) {
+    return (
         <div className="main">
-            <h1>Main</h1>
+            <h1 className="main--name">{props.name}</h1>
+            <h2 className="main--title">{props.title}</h2>
+            <a className="main--site" href={props.site}>{
+                props.site.replace('https://', '')}
+            </a>
+            <div className="main--btn-container">
+                <a  href={`mailto:${props.email}`}>
+                    <button className="main--btn">
+                        <MailIcon className="main--btn-icon"/>
+                        Email
+                    </button>
+                </a>
+                <a  href={props.linkedIn}>
+                    <button className="main--btn main--btn-linkedIn">
+                        <LinkedInIcon className="main--btn-icon"/>
+                        LinkedIn
+                    </button>
+                </a>
+            </div>
         </div>
     );
 }
